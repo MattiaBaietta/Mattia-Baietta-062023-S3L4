06871d2d48b8d7dd5e7095a0b9822a0c71bc7187
MattiaBaietta <mattia.baietta89@gmail.com>
2023-11-09 17:56:04 +0100

    ultima modifica

--- script.js
+++ script.js
@@ -9,53 +9,49 @@
         creare un array per ogni per cartella + 15 h3 controllando che non siano ripetuti per cartella
 
   */
-let numeriestratti=[] //array numeri estratti
-let celletombola=[] //array numeri tra cui poter estrarre
-let cartelle=[[]]
-const posbutton=document.getElementById("NumeroRandom") //posizione bottone per generare il numero random
-console.log(posbutton)
-let caselle=90 //num totale caselle
+let numeriestratti = [] //array numeri estratti
+let celletombola = [] //array numeri tra cui poter estrarre
 
-2
-function PopolaArrayCelle(n){
-    for(let i=1;i<=n;i++)
-    {
+const posbutton = document.getElementById("NumeroRandom") //posizione bottone per generare il numero random
+console.log(posbutton)
+let caselle = 90 //num totale caselle
+
+function PopolaArrayCelle(n) {
+    for (let i = 1; i <= n; i++) {
         celletombola.push(parseInt(i))
     }
 }
 PopolaArrayCelle(caselle)
-console.log(celletombola)
 
-const generanumero= function(){
-        const numrandom=Math.floor(Math.random()*90)
-        if(numeriestratti.includes(numrandom)){
-            window.alert("il numero: "+numrandom +" è già stato estratto")
-        }
-        else{
-            numeriestratti.push(numrandom)
-            console.log(numeriestratti)
-        }
-        let casellahighlight=document.getElementsByClassName(numrandom)
-        console.log(casellahighlight)
-        for(let i=0;i<casellahighlight.length;i++)
-        {
-        casellahighlight[i].style.border= "2px solid red"
-        }       
+
+const generanumero = function () {
+    const numrandom = Math.floor(Math.random() * 90)
+    if (numeriestratti.includes(numrandom)) {
+        window.alert("il numero: " + numrandom + " è già stato estratto")
+    }
+    else {
+        numeriestratti.push(numrandom)
+        console.log(numeriestratti)
+    }
+    let casellahighlight = document.getElementsByClassName(numrandom)
+    console.log(casellahighlight)
+    for (let i = 0; i < casellahighlight.length; i++) {
+        casellahighlight[i].style.border = "2px solid red"
+    }
 }
 
 
 
 
-const creaCelle = function(numeroCaselle){
-    const divtombola=document.getElementById("Tombola")
-    for(let i=0;i<numeroCaselle;i++)
-    {
-        const contenitorecasella=document.createElement("div")
-        contenitorecasella.classList.add (celletombola[i],"divtombola")
-        const casella=document.createElement("h2")
-        casella.innerText=celletombola[i]
-        casella.className="caselle"
-        
+const creaCelle = function (numeroCaselle) {
+    const divtombola = document.getElementById("Tombola")
+    for (let i = 0; i < numeroCaselle; i++) {
+        const contenitorecasella = document.createElement("div")
+        contenitorecasella.classList.add(celletombola[i], "divtombola")
+        const casella = document.createElement("h2")
+        casella.innerText = celletombola[i]
+        casella.className = "caselle"
+
 
         divtombola.appendChild(contenitorecasella)
         contenitorecasella.appendChild(casella)
@@ -65,48 +61,44 @@ creaCelle(caselle)
 
 posbutton.addEventListener("click", generanumero)
 
-const creacartelle = function(){
-        const buttoncartelle=document.getElementById("GenCartelle")
-        buttoncartelle.addEventListener("click",function(){
-        const numCartelle=document.getElementById("NumCartelle")
-        const posizionecartelle=document.getElementById("CartelleGenerate")
-        let provacartella=[]
+//creo un array di array che si riempie con le varie cartelle per verificare i valori
+const creacartelle = function () {
+    const buttoncartelle = document.getElementById("GenCartelle")
+    buttoncartelle.addEventListener("click", function () {
+        const numCartelle = document.getElementById("NumCartelle")
+        const posizionecartelle = document.getElementById("CartelleGenerate")
+        let provacartella = []
         console.log(numCartelle.value)
-        for(let i=0;i<numCartelle.value;i++)
-        {
-            const contenitorecartelle=document.createElement("div")
-            contenitorecartelle.classList="Tombola"
+        for (let i = 0; i < numCartelle.value; i++) {
+            const contenitorecartelle = document.createElement("div")
+            contenitorecartelle.classList = "Tombola"
             posizionecartelle.appendChild(contenitorecartelle)
-            provacartella[i]=[]
-            
-            
-            
-            for(let i2=0;i2<15;i2++)
-            {       
-                
+            provacartella[i] = []
+
+
+
+            for (let i2 = 0; i2 < 15; i2++) {
+
                 contenitorecartelle.classList.add("divcartelle")
-                const contenitorecasella=document.createElement("div")
-                const casella=document.createElement("h2")
-                casella.classList="caselle"
-                let n1cartelle=Math.floor(Math.random()*90)
-                contenitorecasella.classList.add(n1cartelle,"divtombola")
-                if(provacartella[i].includes(n1cartelle))
-                {  
+                const contenitorecasella = document.createElement("div")
+                const casella = document.createElement("h2")
+                casella.classList = "caselle"
+                let n1cartelle = Math.floor(Math.random() * 90)
+                contenitorecasella.classList.add(n1cartelle, "divtombola")
+
+                if (provacartella[i].includes(n1cartelle)) {
                     i2--
 
-                }else{
-                provacartella[i].push(n1cartelle)
-                console.log(provacartella[i][i2])
-                casella.innerText=n1cartelle
-                contenitorecartelle.appendChild(contenitorecasella)
-                contenitorecasella.appendChild(casella)
-                }   
-             }   
-                
+                } else {
+                    provacartella[i].push(n1cartelle)
+                    console.log(provacartella[i][i2])
+                    casella.innerText = n1cartelle
+                    contenitorecartelle.appendChild(contenitorecasella)
+                    contenitorecasella.appendChild(casella)
+                }
+            }
         }
-
-        }
-        
+    }
     )
 }
 
